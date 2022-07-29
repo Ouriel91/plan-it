6fd43025998495807a3151d2f99c149cd0205a28
Orihassid <Orihassid123@gmail.com>
2022-07-29 19:27:23 +0300

    clean folders and arragment

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import thunkMiddleware from "redux-thunk";
-import allReducers from "./reducers/plansRecucer";
+import allReducers from "./redux/reducers/plansRecucer";
 
 export const store = configureStore({
   reducer: allReducers,
