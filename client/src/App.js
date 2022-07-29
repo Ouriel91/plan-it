9e5c336c56d42baec6ee1265206df2fd54836f06
Yulia Kuderko <yulia2232@gmail.com>
2022-07-30 01:04:42 +0300

    layout changes + create plan button change

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import EventPage from './components/EventPage/EventPage';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import ItemList from './components/ItemList/ItemList';
-
+import AboutUsCards from './components/AboutUsCards/AboutUsCards';
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Route path="/">
         <Route index element={<Navbar/>} />
         <Route index element={<ItemList />} />
+        <Route index element={<AboutUsCards />} />
           <Route path="/event-page" element={<EventPage />} />
           <Route index element={<Footer />} />
         </Route>
