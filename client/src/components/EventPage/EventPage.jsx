1c20078060e5b29c4daffd5a0761716f7d8042f1
Kuper-S <kupkup1992@gmail.com>
2022-07-31 10:36:50 +0300

    remove ex2 ex3

--- client/src/components/EventPage/EventPage.jsx
+++ client/src/components/EventPage/EventPage.jsx
@@ -1,6 +1,5 @@
 
 import ItemList from "../ItemList/ItemList";
-import App from "./ex3/App"
 import {BrowserRouter as Router} from 'react-router-dom';
 import EventPageHome from "./EventPageFiles/EventPageHome";
 import { DarkModeContextProvider } from "./EventPageFiles/context/darkModeContext";
