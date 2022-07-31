6db28f991541e6629362590ce2f45b60a6b646d3
Kuper-S <kupkup1992@gmail.com>
2022-07-31 11:49:12 +0300

    remove colordark

--- client/src/components/EventPage/EventPage.jsx
+++ client/src/components/EventPage/EventPage.jsx
@@ -1,14 +1,14 @@
 import EventPageHome from "./EventPageFiles/EventPageHome";
 import { DarkModeContextProvider } from "./EventPageFiles/colormode/darkModeContext";
-
+import Home from "./EventPageFiles/pages/home/Home"
 
 const  EventPage = () => {
   console.log("EventPage")
   return (
     
-      <DarkModeContextProvider>
-      <EventPageHome />
-      </DarkModeContextProvider>
+     
+      <Home  />
+      
     
   
     
