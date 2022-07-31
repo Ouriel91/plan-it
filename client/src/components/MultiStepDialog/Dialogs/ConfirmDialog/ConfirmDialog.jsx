8c5319ffdeafd0ece4830e21bfdc53d0fdcfe429
Kuper-S <kupkup1992@gmail.com>
2022-07-31 10:23:44 +0300

    resolve conflicts

--- client/src/components/MultiStepDialog/Dialogs/ConfirmDialog/ConfirmDialog.jsx
+++ client/src/components/MultiStepDialog/Dialogs/ConfirmDialog/ConfirmDialog.jsx
@@ -8,7 +8,9 @@ import CloseIcon from "@material-ui/icons/Close";
 import AppBar from '@material-ui/core/AppBar';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import { List, ListItem, ListItemText } from '@material-ui/core/';
+import './ConfirmDialog.css'
 import {useNavigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
  
 const Confirm = ({nextStep, prevStep, closeDialog, style, setIsCreateEventClicked, eventObj,addEventAction}) => {
@@ -16,8 +18,7 @@ const Confirm = ({nextStep, prevStep, closeDialog, style, setIsCreateEventClicke
     const handleNextButton = (e) => {
         e.preventDefault();
         nextStep();
-        setIsCreateEventClicked(false)
-        addEventAction(eventObj)
+      
     };
 
     const handlePrevButton = (e) => {
@@ -26,25 +27,35 @@ const Confirm = ({nextStep, prevStep, closeDialog, style, setIsCreateEventClicke
     }  
     
 
-
-  const navigate = useNavigate();
+const navigate = useNavigate();
+  
   
   const navigateToEventPage = () => {
-    // 👇️ navigate to /event-page
-    navigate('/event-page');
+    setIsCreateEventClicked(false)
+    addEventAction(eventObj)
+    navigate('/event-page')
   };
 
     return (
       <MuiThemeProvider>
         <>
           <Dialog
+          PaperProps={{
+            style: {
+              minHeight: '45%',
+              maxHeight: '70%',
+              backgroundColor: "#ffffffd6",
+              borderRadius: "10px",
+              boxShadow: "3px 3px 13px 6px #00000092"
+            }
+          }}
             open
             fullWidth
             maxWidth='sm'
           >
             <DialogTitle>
                 <Grid container justify="space-between" alignItems="center">
-                <Typography variant="div">Event name</Typography>
+                <Typography style={{ color: '#9da275ce', fontFamily: 'Playfair Display', fontSize: '35px' }}>Confirmation</Typography>
                 <IconButton onClick={closeDialog}>
                     <CloseIcon onClick={closeDialog}/>
                 </IconButton>
@@ -53,32 +64,43 @@ const Confirm = ({nextStep, prevStep, closeDialog, style, setIsCreateEventClicke
             <AppBar title="Confirm User Data" />
             <List>
               <ListItem>
-                <ListItemText primary="Event Name" secondary={eventObj.eventName} />
+                <ListItemText 
+                disableTypography
+                primary={<Typography style={{ color: '#b2b887ce', fontFamily: 'Playfair Display', textDecoration: 'underline', fontSize: '17px' }}>Event Name</Typography>} secondary={<Typography style={{ color: 'grey', fontFamily: 'Abel', fontSize: '15px' }}>{eventObj.eventName}</Typography>} />
               </ListItem>
               <ListItem>
-                <ListItemText primary="Dates" secondary={eventObj.dates} />
+                <ListItemText 
+                disableTypography
+                primary={<Typography style={{ color: '#b2b887ce', fontFamily: 'Playfair Display', textDecoration: 'underline', fontSize: '17px' }}>Dates</Typography>} secondary={<Typography style={{ color: 'grey', fontFamily: 'Abel', fontSize: '15px' }}>{eventObj.dates}</Typography>} />
               </ListItem>
               <ListItem>
-                <ListItemText primary="Event Type" secondary={eventObj.eventType} />
+                <ListItemText 
+                disableTypography
+                primary={<Typography style={{ color: '#b2b887ce', fontFamily: 'Playfair Display', textDecoration: 'underline', fontSize: '17px' }}>Event Type</Typography>} secondary={<Typography style={{ color: 'grey', fontFamily: 'Abel', fontSize: '15px' }}>{eventObj.eventType}</Typography>}  />
               </ListItem>
               <ListItem>
-                <ListItemText primary="Occupation" secondary={eventObj.location} />
+                <ListItemText 
+                disableTypography
+                primary={<Typography style={{ color: '#b2b887ce', fontFamily: 'Playfair Display', textDecoration: 'underline', fontSize: '17px' }}>Location</Typography>} secondary={<Typography style={{ color: 'grey', fontFamily: 'Abel', fontSize: '15px' }}>{eventObj.location}</Typography>} />
               </ListItem>
             </List>
             <br />
 
             <div style={style}>
-                <Button
-                color="secondary"
-                variant="contained"
-                onClick={handlePrevButton}
-                >Back</Button>
-
-                <Button
-                color="primary"
-                variant="contained"
-                onClick={navigateToEventPage}
-                >Confirm</Button>
+            <Button
+          style={{ height: "40px", width: "100px", backgroundColor: '#8b8b8bce', fontFamily: 'Playfair Display', letterSpacing: '2px' }}
+          color="secondary"
+          variant="contained"
+          onClick={handlePrevButton}>
+          Back
+        </Button>
+        <Button
+          color="secondary"
+          style={{ height: "40px", width: "100px", backgroundColor: '#98a153ce', fontFamily: 'Playfair Display', letterSpacing: '2px' }}
+          variant="contained"
+          onClick={navigateToEventPage}
+        >Confirm
+        </Button>
             </div>
           </Dialog>
         </>
