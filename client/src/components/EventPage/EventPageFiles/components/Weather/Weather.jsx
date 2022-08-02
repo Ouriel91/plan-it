935423e43ec060c356fad6907a208b8cf2afccb8
Ouriel91 <orieloh91@gmail.com>
2022-08-02 21:21:16 +0300

    finish map and weather

--- client/src/components/EventPage/EventPageFiles/components/Weather/Weather.jsx
+++ client/src/components/EventPage/EventPageFiles/components/Weather/Weather.jsx
@@ -1,15 +1,13 @@
 import {useEffect, useState} from 'react'
-import ReactWeather from 'react-open-weather';
+import ReactWeather, {useOpenWeather} from 'react-open-weather';
 
 const Weather = ({lng, lat, location, date}) => {
-  
   const [data, setData] = useState(
     {
     forecast:[],
     current: {
         date: "01 Aug 2022",
         description: 'Clear',
-        icon:'//cdn.weatherapi.com/weather/64x64/night/116.png',
         temperature: { current: '-2', min: -3, max: 1 },
         wind: '2',
         humidity: 90,
@@ -17,32 +15,26 @@ const Weather = ({lng, lat, location, date}) => {
     } 
   )
 
-  const getFutureWeather = async () => {
-    console.log("hiii11 ")
-    const req = await fetch(`http://api.weatherapi.com/v1/future.json?key=c7fce279755d4f2787c151646220108&q=${lat},${lng}&dt=${date}`)
+  const getFutureWeather = async (lat, lng, date) => {
+    const req = await fetch(`http://api.weatherapi.com/v1/future.json?key=c7fce279755d4f2787c151646220108&q=${+lat},${+lng}&dt=${date}`)
     if(req.statusText === "Bad Request") //can happen because event is less than 14 days or above 300 days, so use 10 days from current date
     {
-      console.log("hiii2d")
-      const newReq = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=c7fce279755d4f2787c151646220108&q=${lat},${lng}&days=10&aqi=no&alerts=no`)
+      const newReq = await fetch( `http://api.weatherapi.com/v1/forecast.json?key=c7fce279755d4f2787c151646220108&q=${+lat},${+lng}&days=10&aqi=no&alerts=no`)
       const res = await newReq.json()
-      console.log("res", res);
       const description = res.current.condition.text
-      const icon = res.current.condition.icon
       const forecast = res.forecast.forecastday[9] //for 10 days from now
       const futureDate = forecast.date + ' (10 days from now)'
-      setResData(description,futureDate,forecast,icon)
+      setResData(description,futureDate,forecast)
       return
     }
-    console.log("hiii3 ")
     const res = await req.json()
     const forecast = res.forecast.forecastday[0]
     const futureDate = forecast.date
     const description = res.forecast.forecastday[0].day.condition.text
-    const icon = res.forecast.forecastday[0].day.condition.icon
-    setResData(description,futureDate,forecast,icon)
+    setResData(description,futureDate,forecast)
   } 
 
-  const setResData = (description,futureDate,forecast,icon) => {
+  const setResData = (description,futureDate,forecast) => {
     const avgTemp = forecast.day.avgtemp_c
     const minTemp = forecast.day.mintemp_c
     const maxTemp = forecast.day.maxtemp_c
@@ -54,30 +46,21 @@ const Weather = ({lng, lat, location, date}) => {
       current: {
           date: futureDate,
           description: description,
-          icon: icon,
           temperature: { current: avgTemp, min: minTemp, max: maxTemp },
           wind: wind,
           humidity: humidity,
         },
     }
-    console.log("overollData",overollData)
     setData(overollData)
   }
 
   useEffect(() => {
-    getFutureWeather()
-  },[])
-  
-  /* const { data, isLoading, errorMessage } = useOpenWeather({
-    key: 'YOUR-API-KEY',
-    lat: '48.137154',
-    lon: '11.576124',
-    lang: 'en',
-    unit: 'metric', // values are (metric, standard, imperial)
-  }) */
+    if(lat === 0 || lng === 0) return
+    getFutureWeather(lat, lng, date)
+  },[lat, lng])
   
   return (
-    <div className='weather'>
+    <div className='weather' style={{width: '40%'}}>
     <ReactWeather
       isLoading={false}
       errorMessage={null}
--- client/src/components/EventPage/EventPageFiles/pages/home/Home.jsx
+++ client/src/components/EventPage/EventPageFiles/pages/home/Home.jsx
@@ -11,9 +11,9 @@ import Footer from "../../../../Footer/Footer";
 // import ImgUploader from "../../components/ImgUploader/ImgUploader,";
 // import UserList from "../../components/UserCard/UserList";
 const Home = ({event}) => {
-  console.log("home",event)
-  const [lat, setLat] = useState("")
-  const [lng, setLng] = useState("")
+  //console.log("home",event)
+  const [lat, setLat] = useState(0.0)
+  const [lng, setLng] = useState(0.0)
   const [date, setDate] = useState("")
   
   const getGeocode = async() => {
@@ -23,8 +23,10 @@ const Home = ({event}) => {
     const data = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`)
     const response = await data.json()
     const results = response.results
-    setLat(results[0].geometry.location.lat)
-    setLng(results[0].geometry.location.lng)
+    const currLat = parseFloat(results[0].geometry.location.lat)
+    const currLng = parseFloat(results[0].geometry.location.lng)
+    setLat(currLat)
+    setLng(currLng)
 
     const orgDate = event.date
     let date = new Date(orgDate).toISOString().slice(0, 10)   
@@ -41,17 +43,15 @@ const Home = ({event}) => {
       <div className="homeContainer">
 
         <Navbar event={event}/>
-        <h1 style={{color: '#000', fontWeight: 'bold'}}>{event.headline}</h1>
-        <h1 style={{color: '#000', fontWeight: 'bold'}}>{event.date}</h1>
-        <div className="EventsTypes">
+        {/* <div className="EventsTypes">
           <EventsType event={event} />
+        </div> */}
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-around', marginTop: '80px'}}>
+          <Location lat={lat} lng={lng} />
+          <Weather lat={lat} lng={lng} location={event.location} date={date}/>
         </div>
-        <Location lat={parseInt(lat)} lng={parseInt(lng)} />
-        <Weather lat={lat} lng={lng} location={event.location} date={date}/>
-        <div className="charts">
           {/* <ImgUploader/> */}
           {/* <UserList/> */}
-        </div>
         {/* <div className="listContainer">
           <div className="listTitle">Event List</div>
           <ItemListConnector event={event} />
