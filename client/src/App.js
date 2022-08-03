c6300e4229f4fb2069fdad848e913f1d6d1fbe08
Yulia Kuderko <yulia2232@gmail.com>
2022-08-03 21:43:00 +0300

    changed background colors

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,29 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EventPage from './components/EventPage/EventPage';
-import CardListConnector from  './components/Card/CardListConnector'
+import CardListConnector from './components/Card/CardListConnector'
 import AppContainerConnector from "./components/AppContainer/AppContainerConnector"
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 function App() {
   return (
     <>
-    <Helmet>
-    <meta charSet="utf-8" />
-    <title>Plan it.</title>
-    <link rel="canonical" href="http://mysite.com/example" />
-</Helmet>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<AppContainerConnector />} />
-          <Route path="/event-page/:id" element={<EventPage />} />
-          <Route path="/my-events" element={<CardListConnector />} />
-          {/* <Route index element={<Footer />} /> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Plan it.</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<AppContainerConnector />} />
+            <Route path="/event-page/:id" element={<EventPage />} />
+            <Route path="/my-events" element={<CardListConnector />} />
+            {/* <Route index element={<Footer />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-    
+
   );
 }
 
--- client/src/components/EventPage/EventPageFiles/components/EventsType/EventsType.jsx
+++ client/src/components/EventPage/EventPageFiles/components/EventsType/EventsType.jsx
@@ -32,7 +32,7 @@ const EventsType = ({ type, setEventObj, event }) => {
   const [timerMins, setTimerMins] = useState('00')
   const [timerSecs, setTimerSecs] = useState('00')
   const [timerEnded, setTimerEnded] = useState(false)
-  const [calendar, setCalendar] = useState(new Date());
+  const [calendar, setCalendar] = useState(new Date(`${event.date}`));
 
 
   const startTimer = () => {
@@ -57,98 +57,109 @@ const EventsType = ({ type, setEventObj, event }) => {
     }, 1000)
   }
 
-    const handleChange = (panel) => (event, isExpanded) => {
-      setExpanded(isExpanded ? panel : false);
-    };
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
-    const handleParticipantsChange = (e) => {
-      setInput(e.target.value);
+  const handleParticipantsChange = (e) => {
+    setInput(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (input === '') {
+      alert('Event can not be empty')
+      return
     }
+    setInput('')
+  }
 
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      if (input === '') {
-        alert('Event can not be empty')
-        return
-      }
-      setInput('')
-    }
+  startTimer()
+  useEffect(() => {
+    Aos.init();
+  }, []);
 
-    startTimer()
-    useEffect(() => {
-      Aos.init();
-    }, []);
+  let image
 
-    let image
+  switch (event.type) {
+    case "BBQ with friends":
+      image = bbq_img
+      break;
+    case "Pool party":
+      image = pool_img
+      break;
+    case "Party":
+      image = party_img
+      break
+    case "Camping":
+      image = camping_img
+      break
+    case "Other":
+      image = other_img
+      break
+    default:
+      image = bbq_img
+  }
 
-    switch (event.type) {
-      case "BBQ with friends":
-        image = bbq_img
-        break;
-      case "Pool party":
-        image = pool_img
-        break;
-      case "Party":
-        image = party_img
-        break
-      case "Camping":
-        image = camping_img
-        break
-      case "Other":
-        image = other_img
-        break
-      default:
-        image = bbq_img
-    }
-
-    return (
-      <>
-        <p className='opening-sentence'>Life is short and the world is wide, <br /> So let's get the best advanture experience we can! </p>
-        <div className='date-counter-holder'>
-          <Calendar
-            onChange={setCalendar}
-            value={calendar}
-            locale="en-GB" />
-          {/* <div class='datepicker'>
+  return (
+    <>
+      <section className='opening-sentence-container'>
+        <p className='opening-sentence'>Life is short and the world is wide, </p>
+        <p className='opening2-sentence'>So let's get the best advanture experience we can!</p>
+      </section>
+      <div className='date-counter-holder'>
+        <Calendar
+          onChange={setCalendar}
+          value={calendar}
+          locale="en-GB" />
+        {/* <div class='datepicker'>
           <div class="datepicker-header"></div>
         </div> */}
-          {/* <text className='date-holder'>{event.date}</text> */}
-          <div className='timer-container'>
-            <section >
-              <p className='date-number'>{timerDays}</p>
-              <p className='date-text'>Days</p>
-            </section>
-            <section>
-              <p className='date-number'>{timerHours}</p>
-              <p className='date-text'>Hours</p>
-            </section>
-            <section>
-              <p className='date-number'>{timerMins}</p>
-              <p className='date-text'>Minutes</p>
-            </section>
-            <section>
-              <p className='date-number'>{timerSecs}</p>
-              <p className='date-text'>Seconds</p>
-            </section>
+        {/* <text className='date-holder'>{event.date}</text> */}
+        <div className='timer-container'>
+          <section >
+            <p className='date-number'>{timerDays}</p>
+            <p className='date-text'>Days</p>
+          </section>
+          <section>
+            <p className='date-number'>{timerHours}</p>
+            <p className='date-text'>Hours</p>
+          </section>
+          <section>
+            <p className='date-number'>{timerMins}</p>
+            <p className='date-text'>Minutes</p>
+          </section>
+          <section>
+            <p className='date-number'>{timerSecs}</p>
+            <p className='date-text'>Seconds</p>
+          </section>
 
-            <img width={500} height={220} id="confetti" alt='party' src={confetti} hidden={!timerEnded ? true : undefined}></img>
-          </div>
+          <img width={500} height={220} id="confetti" alt='party' src={confetti} hidden={!timerEnded ? true : undefined}></img>
         </div>
-        {/* <section data-aos="flip-left" data-aos-offset="100" data-aos-easing="ease-in-sine" data-aos-duration="1000">
+      </div>
+      {/* <section data-aos="flip-left" data-aos-offset="100" data-aos-easing="ease-in-sine" data-aos-duration="1000">
         <div className="listContainer">
           <ItemList />
         </div>
       </section> */}
-        <div className='participants-container'>
-          <UserList />
+      <div className='participants-container'>
+        <UserList />
+      </div>
+
+      <section className='events-type-container'>
+        <div className="listContainer">
+          <div className="listTitle">Event List</div>
+          <NewItemInputConnector event={event} />
+          <ItemListConnector event={event} />
         </div>
-        <section data-aos="zoom-in-down" data-aos-offset="100" data-aos-easing="ease-in-sine" data-aos-duration="1000" className='events-type-container-first-img'>
-          <p data-aos="zoom-in-down" data-aos-offset="100" data-aos-easing="ease-in-sine" data-aos-duration="1000" className='description-event-page'>Sit tight, enjoy a day with your friends and family </p>
-          <p data-aos="zoom-in-down" data-aos-offset="100" data-aos-easing="ease-in-sine" data-aos-duration="1000" className='description2-event-page'> and let us help you with the arrangments</p>
-          <img width={850} height={500} alt={event.type} src={image}></img>
-        </section>
-        {/* <section data-aos="zoom-in-down" data-aos-offset="100" data-aos-easing="ease-in-sine" data-aos-duration="1000" className='events-type-container'> */}
-        {/* <div className="head-section">
+      </section>
+      <section data-aos="zoom-in-down" data-aos-offset="100" data-aos-easing="ease-in-sine" data-aos-duration="1000" className='events-type-container-first-img'>
+        <p data-aos="zoom-in-down" data-aos-offset="100" data-aos-easing="ease-in-sine" data-aos-duration="1000" className='description-event-page'>Sit tight, enjoy a day with your friends and family </p>
+        <p data-aos="zoom-in-down" data-aos-offset="100" data-aos-easing="ease-in-sine" data-aos-duration="1000" className='description2-event-page'>and let us help you with the arrangments</p>
+        <img width={850} height={500} alt={event.type} src={image}></img>
+      </section>
+      {/* <section data-aos="zoom-in-down" data-aos-offset="100" data-aos-easing="ease-in-sine" data-aos-duration="1000" className='events-type-container'> */}
+      {/* <div className="head-section">
           <div className="title-head-section">
             Add friends:
           </div>
@@ -174,7 +185,7 @@ const EventsType = ({ type, setEventObj, event }) => {
           </div>
         </div> */}
 
-        {/* <div className="location-event-page">
+      {/* <div className="location-event-page">
           <Accordion sx={{ width: '300' }} expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
             <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
@@ -194,7 +205,7 @@ const EventsType = ({ type, setEventObj, event }) => {
           </Accordion>
         </div> */}
 
-        {/* <div className="weather-event-page">
+      {/* <div className="weather-event-page">
           <Accordion sx={{ width: '80%' }} expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
             <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
@@ -214,19 +225,11 @@ const EventsType = ({ type, setEventObj, event }) => {
           </Accordion>
         </div> */}
 
-        {/* </div>
+      {/* </div>
 
     </section> */}
-        <section className='events-type-container' data-aos="flip-left" data-aos-offset="100" data-aos-easing="ease-in-sine" data-aos-duration="1000">
-        <div className="listContainer">
-          <div className="listTitle">Event List</div>
-           <NewItemInputConnector event={event} />
-            <ItemListConnector event={event} />
-        </div> 
-        </section>
-
-      </>
-    );
-  };
+    </>
+  );
+};
 
 export default EventsType;
