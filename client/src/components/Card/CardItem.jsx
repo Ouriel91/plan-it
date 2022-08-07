0b66b7754834b059da44d636cb43994e6dfe60e9
Orihassid <Orihassid123@gmail.com>
2022-08-07 22:35:51 +0300

    card item whatsup text

--- client/src/components/Card/CardItem.jsx
+++ client/src/components/Card/CardItem.jsx
@@ -1,67 +1,75 @@
-import bbqImage from '../../images/card cliparts/bbq.jpg'
-import poolImage from '../../images/card cliparts/pool_party.jpg'
-import otherImage from '../../images/card cliparts/other.jpg'
-import campingImage from '../../images/card cliparts/camping.jpg'
-import partyImage from '../../images/card cliparts/party.jpg'
-import waze from '../../images/waze.png'
-import IconButton from '@mui/material/IconButton';
-import ShareIcon from '@mui/icons-material/WhatsApp';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { useNavigate } from 'react-router-dom'
-import "./CardItem.css"
-import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
-import CelebrationOutlinedIcon from '@mui/icons-material/CelebrationOutlined';
-import PoolOutlinedIcon from '@mui/icons-material/PoolOutlined';
-import OutdoorGrillOutlinedIcon from '@mui/icons-material/OutdoorGrillOutlined';
-import ForestOutlinedIcon from '@mui/icons-material/ForestOutlined';
-import SentimentVerySatisfiedOutlinedIcon from '@mui/icons-material/SentimentVerySatisfiedOutlined';
+import bbqImage from "../../images/card cliparts/bbq.jpg";
+import poolImage from "../../images/card cliparts/pool_party.jpg";
+import otherImage from "../../images/card cliparts/other.jpg";
+import campingImage from "../../images/card cliparts/camping.jpg";
+import partyImage from "../../images/card cliparts/party.jpg";
+import waze from "../../images/waze.png";
+import IconButton from "@mui/material/IconButton";
+import ShareIcon from "@mui/icons-material/WhatsApp";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useNavigate } from "react-router-dom";
+import "./CardItem.css";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import CelebrationOutlinedIcon from "@mui/icons-material/CelebrationOutlined";
+import PoolOutlinedIcon from "@mui/icons-material/PoolOutlined";
+import OutdoorGrillOutlinedIcon from "@mui/icons-material/OutdoorGrillOutlined";
+import ForestOutlinedIcon from "@mui/icons-material/ForestOutlined";
+import SentimentVerySatisfiedOutlinedIcon from "@mui/icons-material/SentimentVerySatisfiedOutlined";
 
 const CardItem = ({ plan, deleteEventAction }) => {
+  const navigate = useNavigate();
+  const text = `You are invited to join an event in "Plan it." 
 
-  const navigate = useNavigate()
+  Go check it out now!
+  https://plan-it-monday/event-page/${plan.id.toString()}.herokuapp.com/
+  <br/>
+  Best wishes,
+  
+  Plan it. team :)`;
 
-  let typeStyle = {}
+
+  let typeStyle = {};
   switch (plan.type) {
-    case 'BBQ with friends':
+    case "BBQ with friends":
       typeStyle = {
         primaryColor: "white",
         secondaryColor: "#FDF1F1",
         image: bbqImage,
-        icon: OutdoorGrillOutlinedIcon
-      }
+        icon: OutdoorGrillOutlinedIcon,
+      };
       break;
-    case 'Party':
+    case "Party":
       typeStyle = {
         primaryColor: "white",
         secondaryColor: "#F3F0FD",
         image: partyImage,
-        icon: CelebrationOutlinedIcon
-      }
+        icon: CelebrationOutlinedIcon,
+      };
       break;
-    case 'Pool party':
+    case "Pool party":
       typeStyle = {
         primaryColor: "white",
         secondaryColor: "#ECF3FC",
         image: poolImage,
-        icon: PoolOutlinedIcon
-      }
+        icon: PoolOutlinedIcon,
+      };
       break;
-    case 'Camping':
+    case "Camping":
       typeStyle = {
         primaryColor: "white",
         secondaryColor: "#F2FAF1",
         image: campingImage,
-        icon: ForestOutlinedIcon
-      }
+        icon: ForestOutlinedIcon,
+      };
       break;
-    case 'Other':
+    case "Other":
       typeStyle = {
         primaryColor: "white",
         secondaryColor: "#FEFAF1",
         image: otherImage,
-        icon: SentimentVerySatisfiedOutlinedIcon
-      }
+        icon: SentimentVerySatisfiedOutlinedIcon,
+      };
       break;
 
     default:
@@ -69,12 +77,12 @@ const CardItem = ({ plan, deleteEventAction }) => {
         primaryColor: "",
         secondaryColor: "",
         image: "",
-        icon: ""
-      }
+        icon: "",
+      };
   }
 
   return (
-    <div className='card-item-container'>
+    <div className="card-item-container">
       <div className="card-item">
         <div className="card-box">
           <img
@@ -84,10 +92,14 @@ const CardItem = ({ plan, deleteEventAction }) => {
             width={340}
             src={typeStyle.image}
             alt={plan.type}
-            style={{ objectFit: "cover", borderRadius: "8px 8px 0 0" }} />
+            style={{ objectFit: "cover", borderRadius: "8px 8px 0 0" }}
+          />
         </div>
         <div className="card-content">
-          <div className="card-navigate" onClick={() => navigate(`/event-page/${plan.id}`)}>
+          <div
+            className="card-navigate"
+            onClick={() => navigate(`/event-page/${plan.id}`)}
+          >
             <p className="card-title">{plan.headline}</p>
           </div>
           <div className="card-details">
@@ -103,34 +115,54 @@ const CardItem = ({ plan, deleteEventAction }) => {
             <p className="card-location">{plan.location}</p>
           </div>
           <div className="card-details">
-            <button onClick={() => navigate(`/event-page/${plan.id}`)} className='go-to-btn' type='button'>Go to event</button>
+            <button
+              onClick={() => navigate(`/event-page/${plan.id}`)}
+              className="go-to-btn"
+              type="button"
+            >
+              Go to event
+            </button>
           </div>
           <div className="card-footer">
-            <IconButton aria-label="delete" sx={{ "&:hover": { color: "red" } }} onClick={() => deleteEventAction(plan.id)}>
-              <DeleteIcon className='delete-icon' style={{ fontSize: 30 }} />
+            <IconButton
+              aria-label="delete"
+              sx={{ "&:hover": { color: "red" } }}
+              onClick={() => deleteEventAction(plan.id)}
+            >
+              <DeleteIcon className="delete-icon" style={{ fontSize: 30 }} />
             </IconButton>
             <IconButton aria-label="share">
               {/* send in the future link to heroku event */}
               <a
-                href={`https://web.whatsapp.com/send?text=${plan.headline} - ${plan.type} event in ${plan.location} at ${plan.date} \n`}
+                href={`https://web.whatsapp.com/send?text=${text}`}
                 rel="nofollow noopener noreferrer"
-                target="_blank" 
-                className='whatsapp-icon'>
+                target="_blank"
+                className="whatsapp-icon"
+              >
                 <ShareIcon style={{ fontSize: 30 }} />
               </a>
-            </IconButton>     
-              <a
-                href={`https://waze.com/ul?q=${plan.location.replace('', '%20')}&navigate=yes`}
-                target="_blank"
-                rel='noreferrer'>
-                <img className='waze-icon' src={waze} alt="waze" height={35} width={35} />
-              </a>
+            </IconButton>
+            <a
+              href={`https://waze.com/ul?q=${plan.location.replace(
+                "",
+                "%20"
+              )}&navigate=yes`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="waze-icon"
+                src={waze}
+                alt="waze"
+                height={35}
+                width={35}
+              />
+            </a>
           </div>
         </div>
-
       </div>
     </div>
   );
-}
+};
 
-export default CardItem
+export default CardItem;
